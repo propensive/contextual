bbc283df3e3b5b1380d3e61cdf693fae58106f73
Sam Halliday <sam.halliday@gmail.com>
2017-05-27 21:28:40 +0100

    document hack

--- project/NativeWorkarounds.scala
+++ project/NativeWorkarounds.scala
@@ -9,6 +9,8 @@ object NativeWorkarounds extends AutoPlugin {
   override def trigger = allRequirements
 
   override def projectSettings = Seq(
+    // WORKAROUND https://github.com/propensive/contextual/issues/23
+    scalaVersion := "2.11.11",
     // Scala Native not yet available for 2.12.x
     crossScalaVersions := Seq("2.11.11")
   )
