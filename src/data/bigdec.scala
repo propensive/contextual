b51cce0d0e28de18d5b8694694d4ea2ae9183fd0
Jon Pretty <jon.pretty@propensive.com>
2018-06-04 11:08:42 +0100

    Include BigDecimal example

---
+++ src/data/bigdec.scala
@@ -0,0 +1,33 @@
+/*
+  
+  Contextual, version 1.1.0. Copyright 2018 Jon Pretty, Propensive Ltd.
+
+  The primary distribution site is: https://propensive.com/
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+  this file except in compliance with the License. You may obtain a copy of the
+  License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  License for the specific language governing permissions and limitations under
+  the License.
+
+*/
+package contextual.data
+
+import contextual._
+import scala.util.matching._
+
+object bigDecimal {
+  object BigDecimalParser extends Verifier[BigDecimal] {
+    def check(string: String): Either[(Int, String), BigDecimal] =
+      try Right(BigDecimal(string))
+      catch { case e: NumberFormatException => Left((0, "could not parse decimal")) }
+  }
+
+  implicit class BigDecimalStringContext(sc: StringContext) { val d = Prefix(BigDecimalParser, sc) }
+}
