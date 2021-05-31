ba5354ced721bd4b2a8219b32bb489195c35335b
Jon Pretty <jon.pretty@propensive.com>
2021-05-31 22:35:31 +0200

    Report compile-time errors correctly

--- src/core/contextual.scala
+++ src/core/contextual.scala
@@ -3,7 +3,7 @@ package contextual
 import scala.quoted.*
 import scala.compiletime.*
 
-case class ParseError(str: String) extends Exception()
+case class ParseError(str: String) extends Exception(str)
 
 trait Interpolator[Input, State, Result]:
   def parse(state: State, next: String): State
@@ -14,28 +14,32 @@ trait Interpolator[Input, State, Result]:
   def expand(target: Expr[Interpolator[Input, State, Result]], ctx: Expr[StringContext], seq: Expr[Seq[Any]])
             (using Quotes, Type[Input], Type[State], Type[Result]): Expr[Result] =
     import quotes.reflect.*
+    
     def recur(seq: Seq[Expr[Any]], parts: Seq[String], state: State, expr: Expr[State]): Expr[Result] =
-      try seq match
+      seq match
         case '{ $head: h } +: tail =>
           val typeclass = Expr.summon[Insertion[Input, h]].getOrElse {
             val typeName = TypeRepr.of[h].widen.show
             report.error(s"contextual: can't substitute $typeName into this interpolated string")
             ???
           }
+          
           val newState = parse(insert(state, None), parts.head)
           val newExpr = '{$target.parse($target.insert($expr, Some($typeclass.embed($head))), ${Expr(parts.head)})}
 
           recur(tail, parts.tail, newState, newExpr)
+        
         case _ =>
           '{$target.complete($expr)}
-      catch case error@ParseError(message) =>
-        report.error(s"contextual: $message")
-        throw error
     
     seq match
       case Varargs(exprs) =>
         val parts = ctx.value.get.parts
-        recur(exprs, parts.tail, parse(initial, parts.head), '{$target.parse($target.initial, ${Expr(parts.head)})})
+        try recur(exprs, parts.tail, parse(initial, parts.head), '{$target.parse($target.initial,
+            ${Expr(parts.head)})})
+        catch case error@ParseError(message) =>
+          report.error(s"contextual: $message")
+          throw error
 
 trait Insertion[Input, -T]:
   def embed(value: T): Input
