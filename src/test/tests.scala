d9cf12f9dd920fec675d342fb0a8e511b2921144
Jon Pretty <jon.pretty@propensive.com>
2022-01-20 10:57:29 +0100

    Update version to 0.4.0

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -1,5 +1,5 @@
 /*
-    Contextual, version 2.4.0. Copyright 2016-22 Jon Pretty, Propensive OÜ.
+    Contextual, version 0.4.0. Copyright 2016-22 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
