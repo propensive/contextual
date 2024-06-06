2bc97ceef39ffd30754d9c80f409a78e386e9ab0
Jon Pretty <jon.pretty@propensive.com>
2024-06-06 16:12:55 +0200

    Rename source files and organize imports and `soundness` exports

--- src/core/contextual.Interpolator.scala
+++ src/core/contextual.Interpolator.scala
@@ -16,6 +16,8 @@
 
 package contextual
 
+import language.experimental.captureChecking
+
 import scala.quoted.*
 import scala.compiletime.*
 
@@ -23,23 +25,6 @@ import fulminate.*
 import rudiments.*
 import vacuous.*
 import anticipation.*
-
-import language.experimental.captureChecking
-
-case class InterpolationError(error: Message, offset: Optional[Int] = Unset, length: Optional[Int] = Unset)
-extends Error(msg"$error at ${offset.or(-1)} - ${length.or(-1)}")
-
-trait Verifier[ResultType]
-extends Interpolator[Nothing, Optional[ResultType], ResultType]:
-  def verify(text: Text): ResultType
-  protected def initial: Optional[ResultType] = Unset
-  protected def parse(state: Optional[ResultType], next: Text): Optional[ResultType] = verify(next)
-  protected def skip(state: Optional[ResultType]): Optional[ResultType] = state
-  protected def insert(state: Optional[ResultType], value: Nothing): Optional[ResultType] = state
-  protected def complete(value: Optional[ResultType]): ResultType = value.option.get
-  
-  def expand(context: Expr[StringContext])(using Quotes, Type[ResultType])(using thisType: Type[this.type])
-          : Expr[ResultType] = expand(context, '{Nil})(using thisType)
 
 trait Interpolator[InputType, StateType, ResultType]:
   given CanThrow[InterpolationError] = ###
@@ -51,7 +36,7 @@ trait Interpolator[InputType, StateType, ResultType]:
   protected def substitute(state: StateType, value: Text): StateType = parse(state, value)
   protected def insert(state: StateType, value: InputType): StateType
   protected def complete(value: StateType): ResultType
-  
+
   case class PositionalError(positionalMessage: Message, start: Int, end: Int)
   extends Error(msg"error $positionalMessage at position $start")
 
@@ -60,7 +45,7 @@ trait Interpolator[InputType, StateType, ResultType]:
           : Expr[ResultType] =
 
     expansion(context, seq)(1)
-  
+
   def expansion
       (context: Expr[StringContext], seq: Expr[Seq[Any]])
       (using thisType: Type[this.type])
@@ -89,7 +74,7 @@ trait Interpolator[InputType, StateType, ResultType]:
         case '{$head: headType} +: tail =>
           def notFound: Nothing =
             val typeName: String = TypeRepr.of[headType].widen.show
-            
+
             abandon(msg"can't substitute ${Text(typeName)} into this interpolated string", head.asTerm.pos)
 
           val (newState, typeclass) = Expr.summon[Insertion[InputType, headType]].fold(notFound): insertion =>
@@ -98,36 +83,36 @@ trait Interpolator[InputType, StateType, ResultType]:
                 val substitution: String = (TypeRepr.of[subType].asMatchable: @unchecked) match
                   case ConstantType(StringConstant(string)) =>
                     string
-            
+
                 (rethrow(parse(rethrow(substitute(state, Text(substitution)), expr.asTerm.pos.start, expr.asTerm.pos.end),
                     Text(parts.head)), positions.head.start, positions.head.end), typeclass)
-          
+
               case '{$typeclass: eType} =>
                 (rethrow(parse(rethrow(skip(state), expr.asTerm.pos.start, expr.asTerm.pos.end), Text(parts.head)),
                     positions.head.start, positions.head.end), typeclass)
-            
+
           val next = '{$target.parse($target.insert($expr, $typeclass.embed($head)),
               Text(${Expr(parts.head)}))}
 
           recur(tail, parts.tail, positions.tail, newState, next)
-        
+
         case _ =>
           rethrow(complete(state), Position.ofMacroExpansion.start, Position.ofMacroExpansion.end)
           (state, '{$target.complete($expr)})
-    
+
     val exprs: Seq[Expr[Any]] = seq match
       case Varargs(exprs) => exprs
       case _              => Nil
-        
+
     val parts = context.value.getOrElse:
       abandon(msg"the StringContext extension method parameter does not appear to be inline")
     .parts
-    
+
     val positions: Seq[Position] = (context: @unchecked) match
       case '{(${sc}: StringContext.type).apply(($parts: Seq[String])*)} =>
         (parts: @unchecked) match
           case Varargs(stringExprs) => stringExprs.to(List).map(_.asTerm.pos)
-    
+
     try recur(exprs, parts.tail, positions.tail, rethrow(parse(initial, Text(parts.head)),
         positions.head.start, positions.head.end), '{$target.parse($target.initial, Text(${Expr(parts.head)}))})
     catch
@@ -136,9 +121,3 @@ trait Interpolator[InputType, StateType, ResultType]:
 
       case err: InterpolationError => err match
         case InterpolationError(message, _, _) => abandon(message, Position.ofMacroExpansion)
-      
-trait Insertion[InputType, -ValueType]:
-  def embed(value: ValueType): InputType
-
-trait Substitution[InputType, -ValueType, SubstitutionType <: Label]
-extends Insertion[InputType, ValueType]
